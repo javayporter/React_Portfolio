f1fe4a57e498369a566e536767a64c8c9f5e00c9
javayporter <javayporter@gmail.com>
2020-07-19 19:59:26 -0400

    unable to link PDF added PDF images, will updated in future

--- react_portfolio/src/components/Resume/Resume.js
+++ react_portfolio/src/components/Resume/Resume.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { Grid, Cell} from 'react-mdl';
 
 class Resume extends Component {
     render() {
         return(
-            <div>
-                <h1>Resume Page</h1>
-            </div>
+            <div className="contact-body">
+            <Grid className="contact-grid">
+                <Cell col={6}>
+                
+                    <img
+                    src="https://i.ibb.co/mBCNGqw/2020-07-19-19-42-19.png"
+                    alt="resume-page1"
+                    className="resume-img"
+                    />
+                </Cell>
+                <Cell col={6}>
+                    
+                    <img
+                    src="https://i.ibb.co/p0qS4kb/2020-07-19-19-42-26.png"
+                    alt="resume-page2"
+                    className="resume-img"
+                    />
+                </Cell>  
+            </Grid>
+
+        </div>
         )
     }
 }
