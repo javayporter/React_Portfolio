533e3cb6e565166b177671f80e65a04bae8663b2
javayporter <javayporter@gmail.com>
2020-07-19 18:36:51 -0400

    updated project cards

--- react_portfolio/src/components/Projects/projects.js
+++ react_portfolio/src/components/Projects/projects.js
@@ -21,11 +21,12 @@ class Projects extends Component {
                         <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
                         <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Eat-Da-Burger</CardTitle>
                         <CardText>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Mauris sagittis pellentesque lacus eleifend lacinia...
+                        <p>Eat-Da-Burger! is a restaurant app that lets users input the names of burgers they'd like to eat.</p>
+                        <p>Whenever a user submits a burger's name, your app will display the burger on the left side of the page -- waiting to be devoured.</p>
+                        <p>Each burger in the waiting area also has a Devour it! button. When the user clicks it, the burger will move to the right side of the page.</p>
                         </CardText>
                         <CardActions border>
-                        <a href="https://www.linkedin.com/in/javay-porter" rel="noopener noreferrer" target="_blank">   
+                        <a href="https://github.com/javayporter/Burger" rel="noopener noreferrer" target="_blank">   
                         <Button colored>GitHub Repo</Button>
                         </a>
                         </CardActions>
@@ -43,11 +44,10 @@ class Projects extends Component {
                         <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
                         <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Team Generator</CardTitle>
                         <CardText>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Mauris sagittis pellentesque lacus eleifend lacinia...
+                        Team Generator is a software engineering team generator command line application. The application prompts the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns.
                         </CardText>
                         <CardActions border>
-                        <a href="https://www.linkedin.com/in/javay-porter" rel="noopener noreferrer" target="_blank">   
+                        <a href="https://github.com/javayporter/Team_Generator" rel="noopener noreferrer" target="_blank">   
                         <Button colored>GitHub Repo</Button>
                         </a>
                         </CardActions>
@@ -65,11 +65,10 @@ class Projects extends Component {
                         <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
                         <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Note Taker</CardTitle>
                         <CardText>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Mauris sagittis pellentesque lacus eleifend lacinia...
+                        Note Taker is an application that can be used to write, save, and delete notes. This application uses an express backend and save and retrieve note data from a JSON file.
                         </CardText>
                         <CardActions border>
-                        <a href="https://www.linkedin.com/in/javay-porter" rel="noopener noreferrer" target="_blank">   
+                        <a href="https://github.com/javayporter/Note_Taker" rel="noopener noreferrer" target="_blank">   
                         <Button colored>GitHub Repo</Button>
                         </a>
                         </CardActions>
@@ -87,11 +86,10 @@ class Projects extends Component {
                         <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
                         <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>User Directory</CardTitle>
                         <CardText>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Mauris sagittis pellentesque lacus eleifend lacinia...
+                        React Employee Directory
                         </CardText>
                         <CardActions border>
-                        <a href="https://www.linkedin.com/in/javay-porter" rel="noopener noreferrer" target="_blank">   
+                        <a href="https://github.com/javayporter/User_Directory" rel="noopener noreferrer" target="_blank">   
                         <Button colored>GitHub Repo</Button>
                         </a>
                         </CardActions>
@@ -109,11 +107,16 @@ class Projects extends Component {
                         <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
                         <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Budget App</CardTitle>
                         <CardText>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Mauris sagittis pellentesque lacus eleifend lacinia...
+                        Budget App allows users to add expenses and deposits to their budget with or without a connection. When entering transactions offline, they should populate the total when brought back online. Offline Functionality:
+
+                    <p>* Enter deposits offline</p>
+                    <p>* Enter expenses offline</p>
+                    <p>* When brought back online:</p>
+                    <p>** Offline entries should be added to tracker.</p>
+
                         </CardText>
                         <CardActions border>
-                        <a href="https://www.linkedin.com/in/javay-porter" rel="noopener noreferrer" target="_blank">   
+                        <a href="https://github.com/javayporter/Budget_App" rel="noopener noreferrer" target="_blank">   
                         <Button colored>GitHub Repo</Button>
                         </a>
                         </CardActions>
