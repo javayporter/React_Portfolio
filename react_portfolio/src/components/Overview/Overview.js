493d89598bee9c57420710fb00e3dedc1320f911
javayporter <javayporter@gmail.com>
2020-07-19 20:49:38 -0400

    updated text in Overview file

--- react_portfolio/src/components/Overview/Overview.js
+++ react_portfolio/src/components/Overview/Overview.js
@@ -4,8 +4,11 @@ class Overview extends Component {
     render() {
         return(
             <div>
-                <h1>Resume Page</h1>
-                <p>Javay test</p>
+                <h1>Overview</h1>
+                <p>As an ambitious professional committed to delivering high quality solutions, I throw myself into all task. My diligence, extensive product knowledge,
+and the ability to provide effective solutions led me into my current role as as a Quality Engineer. After months of coding bootcamp I have developed strong critical thinking and technical skills to be proficient in developing/coding. Please review my resume and contact me for any additional information.
+to the team.
+</p>
             </div>
         )
     }
